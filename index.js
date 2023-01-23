144cef29c1dd66d4f1348d9bd8cca84c2827baac
Roshan Dash <roshan1337d@gmail.com>
2023-01-23 19:38:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import getCertificate from './pdf.js';
+import path from 'path';
+import express from 'express'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get("/", (req, res) => res.redirect('https://chat.whatsapp.com/FVfNoDzfnNi5VglrIT9ErG'));
+
+app.get("/certificate", (req, res) => {
+    res.render('home');
+});
+
+app.get("/certificate/:stdid", async (req, res) => {
+    const stdid = req.params.stdid;
+    let stdidTrue = (stdid.match(/[bB][0-9]{6}/g) || [null])[0]
+    if (!stdidTrue) {
+        res.render('error', { message: 'Invalid student ID' });
+        return;
+    }
+    stdidTrue = stdidTrue.toLowerCase()
+    const { valid, data } = await getCertificate(stdidTrue);
+    if (!valid) {
+        res.render('error', {
+            message: 'You haven\'t completed the task yet'
+        });
+        return;
+    }
+    else {
+        res.status(200);
+        res.type('pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=MLSA GitHub Certificate.pdf');
+        res.send(data);
+    }
+});
+
+app.listen(5000, () => console.log('Ready on port 5004...'));
---
+++ pdf.js
@@ -0,0 +1,39 @@
+import fs from 'fs/promises';
+import axios from 'axios';
+import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+
+async function makePDF(id, name) {
+    const pdfData = await fs.readFile('resources/template.pdf');
+    const pdfDoc = await PDFDocument.load(pdfData);
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
+    const { width, height } = firstPage.getSize();
+    firstPage.drawText(name, {
+        x: width * 0.05,
+        y: height * 0.56,
+        size: 35,
+        font: helveticaFont,
+        color: rgb(0, 0.47, 0.83),
+    });
+
+    var pdfBytes = await pdfDoc.save();
+    var pdfBuffer = Buffer.from(pdfBytes.buffer, 'binary');
+    return pdfBuffer;
+}
+
+export default async function getCertificate(stdId) {
+    const filesRes = await axios.get('https://api.github.com/repos/clashverse/Git_Test/git/trees/main?recursive=1');
+    const filesRaw = filesRes.data.tree;
+    for (const f of filesRaw) {
+        const fPath = f.path.toLowerCase();
+        const id = (fPath.match(/[b][0-9]{6}/g) || [null])[0];
+        if (id === stdId.toLowerCase()) {
+            const fRes = await axios.get(f.url);
+            const fContent = atob(fRes.data.content).trim();
+            return { valid: true, data: await makePDF(id, fContent) };
+        }
+    }
+    return { valid: false, data: null };
+}
