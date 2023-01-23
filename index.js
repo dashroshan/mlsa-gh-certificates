85b67ee6620dd5cc6b4a681f4ec3634a987d487a
Roshan Dash <roshan1337d@gmail.com>
2023-01-23 19:52:40 +0530

    order

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get("/", (req, res) => res.redirect('https://chat.whatsapp.com/FVfNoDzfnNi5VglrIT9ErG'));
-
-app.get("/certificate", (req, res) => {
-    res.render('home');
-});
-
 app.get("/certificate/:stdid", async (req, res) => {
     const stdid = req.params.stdid;
     let stdidTrue = (stdid.match(/[bB][0-9]{6}/g) || [null])[0]
@@ -39,5 +33,11 @@ app.get("/certificate/:stdid", async (req, res) => {
         res.send(data);
     }
 });
+
+app.get("/certificate", (req, res) => {
+    res.render('home');
+});
+
+app.get("/", (req, res) => res.redirect('https://chat.whatsapp.com/FVfNoDzfnNi5VglrIT9ErG'));
 
 app.listen(5000, () => console.log('Ready on port 5004...'));
